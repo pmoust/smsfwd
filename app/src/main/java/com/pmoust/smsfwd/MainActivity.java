e76f59bc298365ed4ee0a193a20f0f24b2dd2d55
Panagiotis Moustafellos <pmoust@elastic.co>
2023-04-07 17:49:47 +0300

    Specifying forward address on runtime
    This commit adds the future of specifying forwarding destination phone number
    on runtime.
    Also, added a protection check to not forward an SMS if the set destination
    number is the same from the origin number.
    This avoids an endless loop of sending SMSs in perpetuity.

--- app/src/main/java/com/pmoust/smsfwd/MainActivity.java
+++ app/src/main/java/com/pmoust/smsfwd/MainActivity.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.annotation.NonNull;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.content.pm.PackageManager;
 import android.Manifest;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     private static final int SMS_PERMISSION_REQUEST_CODE = 1;
+    private static final String SHARED_PREFS = "SMSForwarderPrefs";
+    private static final String PHONE_NUMBER_KEY = "destinationPhoneNumber";
+    private EditText phoneNumberInput;
+    private Button savePhoneNumberButton;
 
     private void requestSmsPermissions() {
         String[] permissions = {Manifest.permission.RECEIVE_SMS, Manifest.permission.SEND_SMS};
@@ -23,6 +33,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         requestSmsPermissions();
+
+        phoneNumberInput = findViewById(R.id.phone_number_input);
+        savePhoneNumberButton = findViewById(R.id.save_phone_number_button);
+
+        loadPhoneNumber();
+
+        savePhoneNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                savePhoneNumber();
+            }
+        });
+    }
+
+    private void loadPhoneNumber() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        String phoneNumber = sharedPreferences.getString(PHONE_NUMBER_KEY, "");
+        phoneNumberInput.setText(phoneNumber);
+    }
+
+    private void savePhoneNumber() {
+        String phoneNumber = phoneNumberInput.getText().toString();
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(PHONE_NUMBER_KEY, phoneNumber);
+        editor.apply();
+        Toast.makeText(this, "Phone number saved", Toast.LENGTH_SHORT).show();
     }
 
     @Override
--- app/src/main/java/com/pmoust/smsfwd/SmsReceiver.java
+++ app/src/main/java/com/pmoust/smsfwd/SmsReceiver.java
@@ -3,33 +3,77 @@ package com.pmoust.smsfwd;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+import java.util.Locale;
 
 public class SmsReceiver extends BroadcastReceiver {
-    private static final String FORWARD_TO_PHONE_NUMBER = "6969696969";
+
+    private static final String SHARED_PREFS = "SMSForwarderPrefs";
+    private static final String PHONE_NUMBER_KEY = "destinationPhoneNumber";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Bundle bundle = intent.getExtras();
-        if (bundle != null) {
-            Object[] pdus = (Object[]) bundle.get("pdus");
-            SmsMessage[] messages = new SmsMessage[pdus.length];
-            for (int i = 0; i < pdus.length; i++) {
-                messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-            }
+        if (intent.getAction() != null && intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+            SmsMessage[] messages = extractSmsMessages(intent.getExtras());
             for (SmsMessage message : messages) {
-                String msgBody = message.getMessageBody();
-                String msgFrom = message.getOriginatingAddress();
-                forwardSms(msgFrom, msgBody);
+                String senderPhoneNumber = message.getDisplayOriginatingAddress();
+                String messageText = "From: " + senderPhoneNumber + " " + message.getDisplayMessageBody();
+
+                // Retrieve the destination phone number from SharedPreferences
+                String destinationPhoneNumber = getDestinationPhoneNumber(context);
+
+                if (!destinationPhoneNumber.isEmpty() && !isSamePhoneNumber(senderPhoneNumber, destinationPhoneNumber)) {
+                    // Forward the SMS
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(destinationPhoneNumber, null, messageText, null, null);
+                } else {
+                    // Phone number not set, don't forward the SMS
+                }
             }
         }
     }
 
-    private void forwardSms(String from, String body) {
-        SmsManager smsManager = SmsManager.getDefault();
-        String msg = "From: " + from + " Message: " + body;
-        smsManager.sendTextMessage(FORWARD_TO_PHONE_NUMBER, null, msg, null, null);
+    private SmsMessage[] extractSmsMessages(Bundle bundle) {
+        Object[] pdus = (Object[]) bundle.get("pdus");
+        if (pdus == null || pdus.length == 0) {
+            return new SmsMessage[0];
+        }
+
+        SmsMessage[] messages = new SmsMessage[pdus.length];
+        for (int i = 0; i < pdus.length; i++) {
+            messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+        }
+        return messages;
     }
+
+    private String getDestinationPhoneNumber(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(PHONE_NUMBER_KEY, "");
+    }
+
+    private boolean isSamePhoneNumber(String senderPhoneNumber, String destinationPhoneNumber) {
+        PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+        String defaultRegion = Locale.getDefault().getCountry();
+        try {
+            Phonenumber.PhoneNumber senderNumber = phoneNumberUtil.parse(senderPhoneNumber, defaultRegion);
+            Phonenumber.PhoneNumber destinationNumber = phoneNumberUtil.parse(destinationPhoneNumber, defaultRegion);
+
+            // Strip the country code from both numbers
+            String senderNationalNumber = String.valueOf(senderNumber.getNationalNumber());
+            String destinationNationalNumber = String.valueOf(destinationNumber.getNationalNumber());
+
+            return senderNationalNumber.equals(destinationNationalNumber);
+        } catch (NumberParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
